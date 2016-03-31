895b47b85be3a25878dfa0cf2099ccd74efccfa4
Beyer J <jbeyer@ualberta.ca>
2016-03-31 13:55:09 -0600

    Harvey showed me how to make autoload look into other folders

--- autoload.php
+++ autoload.php
@@ -1,9 +1,22 @@
 <?php
 
+$path = 'C:/Users/Jocelyn/Documents/SchoolWork/Scraper/scraperscripts';
+
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
 function __autoload($classname) {
 	$filename = $classname . '.php';
 	require_once $filename;
+	// had to look 
+	//$filename = '/scraperscripts' . $classname. '.php'; 
+	//require_once $filename;
 }
 
-
+/*
+if (PHP_OS == 'WINNT') {
+			require_once("/../autoload.php");
+		} else {
+			require_once '../autoload.php';
+		}
+*/
 ?>
