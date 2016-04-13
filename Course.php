96cf476c4209d11c23231645bb55cfa06450139f
Beyer J <jbeyer@ualberta.ca>
2016-04-12 20:27:59 -0600

    fixed list of properties

--- Course.php
+++ Course.php
@@ -5,16 +5,16 @@ require_once 'autoload.php';
 
 class Course extends CalendarObject {
 	protected $table = "course";
-	protected $properties = array(
+	public $properties = array(
 			'id' => null,
 			'university_id' => null,
 			'course_title' => null,
 			'course_subtitle' => null,
-			'course_faculty' => null,
+			//'course_faculty' => null,
 			'course_lettercode' => null,
 			'course_number' => null,
 			'course_code' => null,
-			'subject' => null,
+			'course_subject' => null,
 			'term' => null,
 			'year' => null,
 			'campus' => null,
