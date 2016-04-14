b3fd503a7fcabcd84d176f2761b355f75df1f74a
Beyer J <jbeyer@ualberta.ca>
2016-04-14 09:39:12 -0600

    added a method to make the user add properties

--- University.php
+++ University.php
@@ -40,7 +40,20 @@ class University extends CalendarObject {
 
 // save id to university objects's properties
 
+	public function addInfo($ui) {
+		$start = $ui->questionYN(UserInterface::UNI_INFO_PROMPT, "Good Choice\n");
+		if ($start) {
+			foreach ($this->properties as $key => $value) {
+				echo UserInterface::UNI_INFO;
+				$prompt = 'Enter info for ' . $key . "\n";
+				$answer = $ui->userPrompt($prompt);
+				$this->properties[$key] = $answer;
 
+			}
+		//return $this->properties;
+		}
+		
+	}
 
 
 
